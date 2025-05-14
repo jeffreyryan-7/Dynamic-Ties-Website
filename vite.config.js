2c918f3d91207487f022379af7ed383888df02d0
jeffreyryan-7 <jcr101402@gmail.com>
2025-05-13 21:54:35 -0500

    Fixed bugs in site build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/Dynamic-Ties-Website/',
-  plugins: [react()],
-})
+  plugins: [react()]
+});
